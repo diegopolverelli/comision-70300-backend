61858a087d388dced78310ab8315e80bfe66a768
Diego Polverelli <diegopolverelli@hotmail.com>
2025-03-17 22:40:08 -0300

    clase 09 added

---
+++ clase09-20250317-testingUnitario/01-testUnitario/src/dao/Users.dao.js
@@ -0,0 +1,29 @@
+import userModel from "./models/User.js";
+
+
+export default class Users {
+    
+    get = (params) =>{
+        return userModel.find(params);
+    }
+
+    getBy = (params) =>{
+        return userModel.findOne(params);
+    }
+
+    save = (doc) =>{
+        // doc._id="000000000000000000001111"
+        // doc.color="green"
+        // doc.pets=[]
+        // return doc
+        return userModel.create(doc);
+    }
+
+    update = (id,doc) =>{
+        return userModel.findByIdAndUpdate(id,{$set:doc})
+    }
+
+    delete = (id) =>{
+        return userModel.findByIdAndDelete(id);
+    }
+}
